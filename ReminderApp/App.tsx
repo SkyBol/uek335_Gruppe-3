432349eff6358e0dcc7d75b253dc414bc351b4f6
SkyBollin <sky.bollin@noseryoung.ch>
2022-12-20 20:26:08 +0100

    fixed merge

--- ReminderApp/App.tsx
+++ ReminderApp/App.tsx
@@ -1,15 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-<<<<<<< HEAD
 import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
 import { MD3DarkTheme, Provider, Text } from "react-native-paper";
 import PopUpEditor from "./src/components/organism/PopUpEditor";
-=======
-import { Dimensions, StyleSheet, View } from "react-native";
-import { MD3DarkTheme, Provider, Text, useTheme } from "react-native-paper";
 import EditReminderElement from "./src/EditReminderElement";
 import ReminderElement from "./src/ReminderElement";
->>>>>>> 6b71b095d4234fd2a13e7ebd1f50e94e4bfe798a
 
 export default function App() {
   const [isEditScreenOpen, setIsEditScreenOpen] = useState<boolean>(false);
